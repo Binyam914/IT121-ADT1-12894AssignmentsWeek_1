eac222227e91dc94a380a38885d60f011b233a88
Binyam914 <binyamaregwi6@gmail.com>
2024-06-26 08:43:50 -0700

    Add Week 1 assignment files

---
+++ Week_1 - Assignment/app.js
@@ -0,0 +1,20 @@
+// app.js
+
+// Welcome message
+console.log("Hello, world!");
+
+// Variables for status and fear
+let status = "new";
+let scared = false;
+
+// Conditional statement
+if (status === "new") {
+    console.log("Welcome to JavaScript!");
+    if (scared) {
+        console.log("Don't worry you will be fine!");
+    } else {
+        console.log("You're brave! You are going to do great!");
+    }
+} else {
+    console.log("Welcome back, I knew you'd like it!");
+}
